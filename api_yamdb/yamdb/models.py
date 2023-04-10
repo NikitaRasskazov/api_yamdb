79092a706444b99bfa4e9315fddf8c15e7d917ba
Ludmila Morozova <vzbodris@mail.ru>
2023-04-11 00:55:18 +0300

    add GenreTitle through model

--- api_yamdb/yamdb/models.py
+++ api_yamdb/yamdb/models.py
@@ -9,6 +9,14 @@ class Category(models.Model):
         return self.name
 
 
+class Genre(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Title(models.Model):
     name = models.CharField(max_length=200)
     year = models.DateTimeField()  # во views -> year.year или сделать через IntegerField
@@ -19,14 +27,21 @@ class Title(models.Model):
         blank=True,
         null=True,
     )
+    genre = models.ManyToManyField(
+        Genre,
+        through='GenreTitle',
+    )
 
     def __str__(self):
         return self.name
 
 
-class Genre(models.Model):
-    name = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
-
-    def __str__(self):
-        return self.name
+class GenreTitle(models.Model):
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=models.CASCADE,
+    )
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+    )
