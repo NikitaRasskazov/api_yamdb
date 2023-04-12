5a0e92a6c1cb4e24e2907981e47f94d1b261b724
Ludmila Morozova <vzbodris@mail.ru>
2023-04-12 13:41:34 +0300

    register CategoryView

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import GenreViewSet
+from .views import CategoryViewSet, GenreViewSet
 
 router = DefaultRouter()
 
+router.register('categories', CategoryViewSet,)
 router.register('genres', GenreViewSet,)
 
 urlpatterns = [
