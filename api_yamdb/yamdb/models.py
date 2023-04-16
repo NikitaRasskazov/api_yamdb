c9400446083c4f4ac7b747368d4ed2e33d68edb8
Ludmila Morozova <vzbodris@mail.ru>
2023-04-17 00:27:04 +0300

    fix Category, Genre and Title models

--- api_yamdb/yamdb/models.py
+++ api_yamdb/yamdb/models.py
@@ -5,23 +5,23 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(unique=True, max_length=50)
 
     def __str__(self):
         return self.name
 
 
 class Genre(models.Model):
-    name = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(unique=True, max_length=50)
 
     def __str__(self):
         return self.name
 
 
 class Title(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=256)
     year = models.IntegerField(
         validators=[
             MinValueValidator(1, message='Год должен быть натуральным числом'),
