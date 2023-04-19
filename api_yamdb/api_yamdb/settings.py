411ab87c5afaf66b2c73f846cf8749ef3feb6d8b
Никита Рассказов <vozakssar@yandex.ru>
2023-04-20 01:44:24 +0500

    add file apps

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -23,7 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework_simplejwt',
-    'api',
+    'api.apps.ApiConfig',
     'reviews.apps.ReviewsConfig',
     'rest_framework',
     'django_filters',
